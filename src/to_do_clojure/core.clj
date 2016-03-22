7021ef817f10b88e5f2aedd4234a69d041ad8e61
ericweidman <ericweidman@gmail.com>
2016-03-22 11:13:48 -0400

    Initial import

---
+++ src/to_do_clojure/core.clj
@@ -0,0 +1,15 @@
+(ns to-do-clojure.core
+  (:gen-class))
+
+(def todos atom[]) ; semicolon to comment 
+
+
+(defn -main []
+  (println "Type a to-do and hit enter. Type q to quit.")
+  (loop []
+    (let [text (read-line)]
+      (when (not= text "q")
+        (reset! todos (conj @todos text))
+        (recur))))
+  (println @todos))
+
